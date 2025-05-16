d988bfd1a74fdce2810e8ec87df02f60eaba6469
Ziya Celebi <ziyacelebi30@gmail.com>
2025-05-16 07:18:55 +0530

    Update

--- src/main.js
+++ src/main.js
@@ -15,9 +15,24 @@ let days = [
   "Sunday",
 ];
 
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 28; i++) {
   grid[i] = document.createElement("button");
   grid[i].className = "bunny-button";
-  grid[i].textContent = "I love you";
+
   document.body.appendChild(grid[i]);
 }
+
+for (let k = 0; k < 4; k++) {
+  for (let j = 0; j < 7; j++) {
+    grid[7 * k + j].textContent = days[j];
+  }
+}
+
+function task() {
+  grid[0].addEventListener("click", () => {
+    const userInput = prompt("Enter your task");
+    grid[0].textContent = grid[0].textContent + " " + userInput;
+  });
+}
+
+task();
